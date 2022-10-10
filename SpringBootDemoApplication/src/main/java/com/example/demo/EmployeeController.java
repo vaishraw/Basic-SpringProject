f156b024be5932bce288914029cbeace1b17a56a
Vaishnavi <vaishnavirawat2902@gmail.com>
2022-10-10 16:57:59 +0530

    changes

---
+++ SpringBootDemoApplication/src/main/java/com/example/demo/EmployeeController.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+ 	
+@RestController
+public class EmployeeController 
+{
+   @RequestMapping("/")
+    public List<Employee> getEmployees() 
+{
+      List<Employee> employeesList = new ArrayList<Employee>();
+      employeesList.add(new Employee(1,"lokesh","gupta","howtodoinjava@gmail.com"));
+      return employeesList;
+    }
+}
+
+
